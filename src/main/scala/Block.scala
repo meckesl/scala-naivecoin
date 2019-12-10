46a1d21e79bcc926fcf849b466aa0d286a23d514
Louis Meckes <louis.meckes@gmx.com>
2019-12-10 08:31:34 +0100

    chain validation

--- src/main/scala/Block.scala
+++ src/main/scala/Block.scala
@@ -3,16 +3,13 @@ case class Block(
   previousHash: String,
   timestamp: Long,
   data: String
-) {
+)
+{
+
   def hash =
     Block.sha256Hash(index+previousHash+timestamp+data)
-}
 
-object Block {
-
-  val genesisBlock = Block(0, "", 0, "")
-
-  def generate(previousBlock: Block, data: String): Block = {
+  def generate(data: String, previousBlock: Block = this): Block = {
     import java.util.Date
     Block(
       previousBlock.index +1,
@@ -22,16 +19,44 @@ object Block {
     )
   }
 
+  override def toString: String = {
+    s"index=${index}, hash=$hash, previousHash=$previousHash, timestamp=$timestamp, data=$data"
+  }
+
+}
+
+object Block {
+
+  val genesisBlock = Block(0, "", 0, "")
+  val sha256 = java.security.MessageDigest.getInstance("SHA-256")
+
   def sha256Hash(text: String) : String =
     String.format("%064x",
       new java.math.BigInteger(1,
-        java.security.MessageDigest.getInstance("SHA-256")
-        .digest(text.getBytes("UTF-8"))))
+        sha256.digest(text.getBytes("UTF-8"))))
+
+  def validate(_old: Block, _new: Block): Option[Block] =
+    (_new.previousHash.equals(_old.hash)) match {
+      case true => Some(_new)
+      case _ => None
+    }
 
   def main(args: Array[String]): Unit = {
 
+    val block1 = Block.genesisBlock.generate("lol")
+    val block2 = block1.generate("pouet")
+    val block3 = block2.generate("pouet pouet")
+    val block4 = block1.generate("noo")
+    val blockChain: Seq[Block] = Seq(block1, block2, block3, block4)
 
-    val blockChain = Seq(Block.genesisBlock)
+    val validatedBlockChain = blockChain.foldLeft(Seq[Option[Block]]())((acc, b) => {
+      acc match {
+        case x::xs if x.nonEmpty => validate(x.get,b) +: acc
+        case _ => acc:+validate(genesisBlock, b)
+      }
+    })
+
+    print(validatedBlockChain.map(_.toString).mkString("\n"))
 
   }
 
